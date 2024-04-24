1ff104bfd84dbb75f00a4fa72bf392a3b49d0ac7
usiqwerty <benlogin1@protonmail.com>
2024-04-24 14:08:02 +0500

    Управление стрелками

--- Assets/Scenes/PlayerControl.cs
+++ Assets/Scenes/PlayerControl.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// using Input = UnityEngine.Windows.Input;
+
 public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -15,8 +17,19 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var absSpeed = 10;
+        var newspeed = new Vector2 (0, 0);
+        if (Input.GetKey(KeyCode.UpArrow))
+            newspeed.y = absSpeed;
+        if (Input.GetKey(KeyCode.DownArrow))
+            newspeed.y = -absSpeed;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            newspeed.x = -absSpeed;
+        if (Input.GetKey(KeyCode.RightArrow))
+            newspeed.x = absSpeed;
+            
         var old = transform.position;
-        transform.position = new Vector2(old.x + speed * Time.deltaTime, old.y+speed * Time.deltaTime);
+        transform.position = new Vector2(old.x + newspeed.x * Time.deltaTime, old.y + newspeed.y * Time.deltaTime);
     
     }
 }
