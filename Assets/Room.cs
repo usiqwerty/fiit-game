c1fcb7726b043c1b593254bb9b305d85b7cdaf84
usiqwerty <benlogin1@protonmail.com>
2024-04-25 15:34:26 +0500

    Добавлен класс комнат

---
+++ Assets/Room.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * <summary>Комната</summary>
+ */
+public class Room : MonoBehaviour
+{
+    /**
+     * <summary>Имя комнаты, которое отображается</summary>
+     */
+    public string Name;
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
