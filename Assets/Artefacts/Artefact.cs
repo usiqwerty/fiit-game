0884e74cc16b5aea54025d5bff130afed5f17fa0
usiqwerty <benlogin1@protonmail.com>
2024-05-01 23:20:04 +0500

    База для артефактов

---
+++ Assets/Artefacts/Artefact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Scenes
+{
+    public enum ArtefactName
+    {
+        Caltulator,
+        Differential,
+    }
+    public class Artefact : MonoBehaviour
+    {
+        public ArtefactName type;
+        public bool spawnOnStart;
+    }
+}
