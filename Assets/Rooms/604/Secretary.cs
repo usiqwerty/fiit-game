ae167ec953e58e784f651ea510e14a4963f07a75
NeSawer <danilch4024@gmail.com>
2024-05-30 18:09:40 +0500

    Secretary message box

--- Assets/Rooms/604/Secretary.cs
+++ Assets/Rooms/604/Secretary.cs
@@ -6,23 +6,30 @@ public class Secretary : MonoBehaviour
     private DoorScript _deanDoor;
     private GameObject _player;
 
+    private bool _isShow = false;
+    private MessageBoxEntry _messageBox;
+
     void Start()
     {
         _allowIn = ArtefactStorage.ContainsKeys(ArtefactStorage.Required);
         _deanDoor = GameObject.FindGameObjectWithTag("DeanDoor").GetComponent<DoorScript>();
         _deanDoor.RequiredKeys = ArtefactStorage.Required;
         _player = GameObject.FindGameObjectWithTag("Player");
+        _messageBox = GameObject.Find("MessageBox").GetComponent<MessageBox>().CreateEntry();
     }
-    
-    private void OnGUI()
+
+    private void Update()
     {
-        if ((_player.transform.position - transform.position).magnitude < 3)
+        if (_isShow != (_player.transform.position - transform.position).sqrMagnitude < 9)
         {
+            _isShow = !_isShow;
             if (!_allowIn)
             {
-                GUI.Label(new Rect(100, 100, 200, 200), "Вы же не собрали всё, что нужно! Приходите, когда будете готовы к пересдаче");
+                if (_isShow)
+                    _messageBox.Activate("Вы же не собрали всё, что нужно! Приходите, когда будете готовы к пересдаче");
+                else
+                    _messageBox.Disable();
             }
-            
         }
     }
 }
